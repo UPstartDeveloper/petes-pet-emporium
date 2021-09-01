f035b1d20328529390700a6c4b6182ca5aeb2a3c
= <zainr7989@gmail.com>
2021-09-01 16:13:36 -0400

    Implement validation.

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -14,7 +14,16 @@ if (document.querySelector('#new-pet')) {
                 window.location.replace(`/pets/${response.data.pet._id}`);
             })
             .catch(function (error) {
-                console.log(error);
+                // reveal Bootstrap alert
+                const alert = document.getElementById('alert')
+                alert.classList.add('alert-warning');
+                alert.textContent = 'Oops, something went wrong saving your pet. Please check your information and try again.';
+                alert.style.display = 'block';
+                // make the alert disappear again
+                setTimeout(() => {
+                    alert.style.display = 'none';
+                    alert.classList.remove('alert-warning');
+                }, 3000);
             });
     });
 }
